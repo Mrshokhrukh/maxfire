89234dfa916768180d2260d6c51e242e90c5693d
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-06-25 18:34:13 +0500

    home page circle text

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./homepage.scss";
+import { BsArrowDown } from "react-icons/bs";
 const HomePage = () => {
+  useEffect(() => {
+    const text = document.querySelector(".circle_text p");
+    text.innerHTML = text.innerText
+      .split("")
+      .map(
+        (char, i) =>
+          `<span style='transform:rotate(${i * 6.4}deg)'>${char}</span>`
+      )
+      .join("");
+  }, []);
+
   return (
     <div className="homepage">
-     
-        <div className="homepage_text">
+      <div className="homepage_text">
+        <div>
           <h1>
-            bmnskjlkalkienneraise <br /> hfdksnddklsk idnslm
+            высококачественний <br /> древесный уголь
           </h1>
+          <div className="adv_text">
+            <p>Без дыма и запаха</p>
+          </div>
         </div>
+
+        <div className="adv_exp">
+          <p>10+ лет в продаже</p>
+        </div>
+      </div>
+      <div className="to_bottom_circled_text">
+        <div className="to_bottom_icon">
+          <BsArrowDown />
+        </div>
+        <div className="circle_text">
+          <p>листавйте вниз * листавйте вниз * </p>
+        </div>
+      </div>
     </div>
   );
 };
