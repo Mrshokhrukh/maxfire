4865621799183e53d360cae300810fb131fc3d0e
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-07-06 14:42:57 +0500

    cxc

--- src/components/infoBoxTwo/InfoBoxTwo.jsx
+++ src/components/infoBoxTwo/InfoBoxTwo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./InfoBoxTwo.scss";
 import blured from "../../assets/shape.png";
+import { BsArrowRight } from "react-icons/bs";
 const InfoBoxTwo = () => {
   return (
     <div className="desc_infogram">
@@ -10,10 +11,18 @@ const InfoBoxTwo = () => {
         <span className="starter_text">Стартер </span> - для розжига угля
       </h1>
       <div className="infogram_text">
-        <p>изготовлен из качественного металла толщиной 0,5 мм</p>
-        <p>стильная и удобная ручка из дуба не порежет пальцы</p>
-        <p>диаметр стартера 18 см, высота 30 см</p>
-        <p>идеальный объем для 2 кг брикета</p>
+        <p>
+          <BsArrowRight /> изготовлен из качественного металла толщиной 0,5 мм
+        </p>
+        <p>
+          <BsArrowRight /> стильная и удобная ручка из дуба не порежет пальцы
+        </p>
+        <p>
+          <BsArrowRight /> диаметр стартера 18 см, высота 30 см
+        </p>
+        <p>
+          <BsArrowRight /> идеальный объем для 2 кг брикета
+        </p>
       </div>
     </div>
   );
